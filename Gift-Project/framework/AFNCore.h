40d5877291a4ef798a7c6e00ecf195c72ee6637b
xiaoxi <366998502@qq.com>
2016-06-06 10:17:30 +0800

    孙婷婷：登录注册

---
+++ Gift-Project/framework/AFNCore.h
@@ -0,0 +1,20 @@
+//
+//  AFNCore.h
+//  Gift-Project
+//
+//  Created by mac on 16/6/6.
+//  Copyright © 2016年 tingting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^MyBlock)(id result);
+
+@interface AFNCore : NSObject
+
++ (void)registerByEmail:(NSString *)email byPassword:(NSString *)password byMyBlock:(MyBlock)block;
+
++ (void)loginByEmail:(NSString *)email byPassword:(NSString *)password byMyBlock:(MyBlock)block;
+
+
+@end
---
+++ Gift-Project/framework/AFNCore.m
@@ -0,0 +1,66 @@
+//
+//  AFNCore.m
+//  Gift-Project
+//
+//  Created by mac on 16/6/6.
+//  Copyright © 2016年 tingting. All rights reserved.
+//
+
+#import "AFNCore.h"
+#import <Maxleap/MaxLeap.h>
+
+@implementation AFNCore
+
++ (void)registerByEmail:(NSString *)email byPassword:(NSString *)password byMyBlock:(MyBlock)block{
+    
+    MLUser *user = [MLUser user];
+    
+    user.username = email;
+    
+    user.password = password;
+    
+    user.email = email;
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        
+        if (succeeded) {
+            
+            NSLog(@"注册成功");
+            
+            block(@"注册成功");
+            
+        } else {
+            
+            NSString *errorString = [error userInfo][@"NSLocalizedDescription"];
+            
+            NSString *error = [errorString stringByReplacingOccurrencesOfString:@"用户名" withString:@"邮箱"];
+            
+            NSLog(@"注册失败,错误为：%@",error);
+            
+            block(error);
+        }
+    }];
+    
+    
+}
+
+
++ (void)loginByEmail:(NSString *)email byPassword:(NSString *)password byMyBlock:(MyBlock)block{
+    
+    [MLUser logInWithUsernameInBackground:email password:password block:^(MLUser *user, NSError *error) {
+        
+        if (user) {
+            
+            block(@"登录成功");
+            
+        } else {
+            
+            block([error userInfo][@"NSLocalizedDescription"]);
+            
+        }
+    }];
+}
+
+
+
+@end
